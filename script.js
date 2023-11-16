bdd2c0f35e6b692ca18bd464d070d719edc2c13d
callbackKid <vladislav.yarosh23@gmail.com>
2023-11-16 16:21:03 +0400

    filter

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ usersContainer.classList.add('user-container')
 root.append(usersContainer)
 
 const buttonTheme = document.querySelector('#change-theme')
-// get - получить взять брать
 const currentTheme = localStorage.getItem('theme')
 
 if (currentTheme) {
   document.body.classList.add(currentTheme)
 }
-// parse из JSON конвертировать в обычный объект
 const userFromStorage = JSON.parse(localStorage.getItem('userData'))
 
 if (userFromStorage) {
@@ -26,8 +24,6 @@ buttonTheme.addEventListener('click', () => {
   localStorage.setItem('theme', document.body.classList)
 })
 
-// надо достать данные массива по ключу, распарсить и занести в переменную
-// если у нас в LS ничего нет по ключу то подставляем пустой массив
 const userArray = JSON.parse(localStorage.getItem('userArray')) || []
 
 const showUsers = (arr) => {
@@ -48,22 +44,39 @@ showUsers(userArray)
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const user = {
     name: nameInput.value,
     age: ageInput.value,
   }
-
   localStorage.setItem('userData', JSON.stringify(user))
-
-  userArray.push(user) // изменяю массив
-  // добавляю в LS
+  userArray.push(user)
   localStorage.setItem('userArray', JSON.stringify(userArray))
-
   showUsers(userArray)
   nameInput.value = ''
   ageInput.value = ''
 })
 
-// setItem => stringify - добавляем в LS (в LS только строки)
-// getItem => parse() - получаем из LS
+// SEARCH INPUT
+const searchLabel = document.createElement('label')
+searchLabel.setAttribute('for', 'search')
+searchLabel.innerText = 'search'
+root.prepend(searchLabel)
+
+const searchInput = document.createElement('input')
+searchInput.type = 'text'
+searchInput.id = 'search'
+root.prepend(searchInput)
+
+searchInput.addEventListener('input', (event) => {
+  // ... spread operator - создаем копию массива
+  // копия для того чтобы не мутировать исходный массив
+  // const newArray = [...userArray]
+
+  const filteredArray = userArray.filter((user) =>
+    user.name.toLowerCase().includes(event.target.value.toLowerCase())
+  )
+
+  showUsers(filteredArray)
+  // при вводе данных проверяем содержит ли наш элемент(его имя) подстроку
+  // и результат перерисовываем
+})
