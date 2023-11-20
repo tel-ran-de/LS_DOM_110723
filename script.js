0a5448dd89d378000f896144b426deafe3143408
callbackKid <vladislav.yarosh23@gmail.com>
2023-11-21 01:20:00 +0400

    better ui

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-const form = document.querySelector('form') // по тегу
-const root = document.querySelector('#root') // по id #
+const form = document.querySelector('form')
+const header = document.querySelector('header')
+const root = document.querySelector('#root')
 const nameInput = document.querySelector('#name')
 const ageInput = document.querySelector('#age')
 const usersContainer = document.createElement('div')
 usersContainer.classList.add('user-container')
 root.append(usersContainer)
 
+// THEME
 const buttonTheme = document.querySelector('#change-theme')
 const currentTheme = localStorage.getItem('theme')
-
-if (currentTheme) {
-  document.body.classList.add(currentTheme)
-}
-const userFromStorage = JSON.parse(localStorage.getItem('userData'))
-
-if (userFromStorage) {
-  nameInput.value = userFromStorage.name
-  ageInput.value = userFromStorage.age
-}
 
 buttonTheme.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme')
   localStorage.setItem('theme', document.body.classList)
 })
+
+if (currentTheme) {
+  document.body.classList.add(currentTheme)
+}
+
+// SEARCH INPUT
+const searchLabel = document.createElement('label')
+searchLabel.setAttribute('for', 'search')
+searchLabel.innerText = 'search'
+header.append(searchLabel)
+
+const searchInput = document.createElement('input')
+searchInput.type = 'text'
+searchInput.id = 'search'
+header.append(searchInput)
 
 const userArray = JSON.parse(localStorage.getItem('userArray')) || []
 
@@ -48,24 +55,13 @@ form.addEventListener('submit', (event) => {
     name: nameInput.value,
     age: ageInput.value,
   }
-  localStorage.setItem('userData', JSON.stringify(user))
+
   userArray.push(user)
   localStorage.setItem('userArray', JSON.stringify(userArray))
   showUsers(userArray)
   nameInput.value = ''
   ageInput.value = ''
 })
-
-// SEARCH INPUT
-const searchLabel = document.createElement('label')
-searchLabel.setAttribute('for', 'search')
-searchLabel.innerText = 'search'
-root.prepend(searchLabel)
-
-const searchInput = document.createElement('input')
-searchInput.type = 'text'
-searchInput.id = 'search'
-root.prepend(searchInput)
 
 searchInput.addEventListener('input', (event) => {
   // ... spread operator - создаем копию массива
